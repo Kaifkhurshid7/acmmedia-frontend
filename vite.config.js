831eb7150abc968cd4273cafe7ae7aa5cb03ea2f
kaifkhurshid7 <kaifkhurshid18@gmail.com>
2026-01-14 12:34:50 +0530

    added vite config

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ['sk96ps-5173.csb.app'],
+  },
+})
